f00f259b212e04e60fdaa05f6b61f2cb1200f486
rehan karnani <rehankarnani@gmail.com>
2022-07-08 10:20:08 -0400

    basic data types.js

---
+++ Unit Testing Activity/story_time.js
@@ -0,0 +1,34 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings ="Welcome to Florida";
+let name = "Braxton the Second";
+let title= "King of Crazy Shizz";
+let worldName = "Florida goes Crazy";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 28;
+let year = 2050;
+let century = 21;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let itemsUsed = ['phone','credit card', 'jets','fighter jet'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavItem = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + ". My name is "+ name + ".");
+console.log("I'm the " +  title + ".");
+console.log("As the leader of " + worldName + ", I use different items.");
+console.log("These items are used to do crazy shizz in Florida.");
+console.log("For example I am " + age + " years old.")
+console.log("In my world you came into year "+ year + " and " + century + " is the century you are in.");
+console.log("Does anyone like flying " + itemsUsed[2] +  " like Tom Cruise in Top Gun?");
+console.log("Me: I love his stunts and how he flys really low!");
+console.log("When someone asks, do you love your favorite item, I always respond with " + hasFavItem + ".");
+console.log("Well that is what I " + name + " love to do. I'm going to Miami to pull this off!");
+console.log("Friends: Be Careful! "+ name + ", good luck!");
